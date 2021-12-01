d3c2cc4f6f4c893edb94a24b0921e021b4ec94ef
dimitry@rebelit.nl <dimitry@rebelit.nl>
2021-12-01 23:40:31 +0100

    Ran code cleanup

--- Puzzle1/Solution.fs
+++ Puzzle1/Solution.fs
@@ -1,36 +1,34 @@
 ﻿module AoC2021.Puzzle1.Solution
+
 open System.IO
-    
-let start_solution_1() =
+
+let start_solution_1 () =
     let mutable count = 0
     let fileLines = File.ReadLines("Puzzle1\input.txt")
     let data = fileLines |> Seq.map int |> Seq.pairwise
-    
-    
+
     for first, second in data do
-        match (first,second) with
-            | (first, second) when first < second -> count <- count + 1
-            | _ -> ()
-            
-    printfn "%A" count
-    
-let start_solution_1_part_two() =
-    let mutable count = 0
-    let fileLines = File.ReadLines("Puzzle1\input.txt")
-    let windowedData =
-        fileLines
-            |> Seq.map int
-            |> Seq.windowed 3
-            |> Seq.map (fun window  -> window |> Array.sum)
-            |> Seq.pairwise               
-   
-    
-    for first, second in windowedData do
-        match (first,second) with
-            | (first, second) when first < second -> count <- count + 1
-            | _ -> ()
+        match (first, second) with
+        | (first, second) when first < second -> count <- count + 1
+        | _ -> ()
 
     printfn "%A" count
-    
-    
-    
+
+let start_solution_1_part_two () =
+    let mutable count = 0
+    let fileLines = File.ReadLines("Puzzle1\input.txt")
+
+    let windowedData =
+        fileLines
+        |> Seq.map int
+        |> Seq.windowed 3
+        |> Seq.map (fun window -> window |> Array.sum)
+        |> Seq.pairwise
+
+
+    for first, second in windowedData do
+        match (first, second) with
+        | (first, second) when first < second -> count <- count + 1
+        | _ -> ()
+
+    printfn "%A" count
