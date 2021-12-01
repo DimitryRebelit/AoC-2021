95730fa7f58578c364387d5fdd29c3c845928c34
dimitry@rebelit.nl <dimitry@rebelit.nl>
2021-12-01 23:33:24 +0100

    Updated project with part two of puzzle 1

--- Program.fs
+++ Program.fs
@@ -1,5 +1,8 @@
 ﻿open AoC2021.Puzzle1.Solution
 
 // For more information see https://aka.ms/fsharp-console-apps
-
+printfn "== Solution 1 == "
 start_solution_1()
+
+printf "Part two"
+start_solution_1_part_two()
--- Puzzle1/Solution.fs
+++ Puzzle1/Solution.fs
@@ -2,15 +2,35 @@
 open System.IO
     
 let start_solution_1() =
-    
+    let mutable count = 0
     let fileLines = File.ReadLines("Puzzle1\input.txt")
     let data = fileLines |> Seq.map int |> Seq.pairwise
     
-    let mutable ascending = 0
     
     for first, second in data do
         match (first,second) with
-            | (first, second) when first > second -> ascending <- ascending + 1
+            | (first, second) when first < second -> count <- count + 1
             | _ -> ()
             
-    printfn "%A" ascending 
+    printfn "%A" count
+    
+let start_solution_1_part_two() =
+    let mutable count = 0
+    let fileLines = File.ReadLines("Puzzle1\input.txt")
+    let windowedData =
+        fileLines
+            |> Seq.map int
+            |> Seq.windowed 3
+            |> Seq.map (fun window  -> window |> Array.sum)
+            |> Seq.pairwise               
+   
+    
+    for first, second in windowedData do
+        match (first,second) with
+            | (first, second) when first < second -> count <- count + 1
+            | _ -> ()
+
+    printfn "%A" count
+    
+    
+    
