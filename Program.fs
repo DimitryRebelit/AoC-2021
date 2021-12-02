04cc6f3ffd09381003d1e1cae5c3dd682e5c38b0
dimitry@rebelit.nl <dimitry@rebelit.nl>
2021-12-02 09:34:17 +0100

    Finished Part One of Solution 2

--- Program.fs
+++ Program.fs
@@ -1,7 +1,2 @@
-﻿(*
-    Console application for running all the solutions to the Advent of Code 2021 puzzles
-    Author:         Dimitry Volker
-    Organisation:   Rebel:it
-*)
-
-AoC2021.Puzzle1.Solution.Start()
+﻿AoC2021.Puzzle1.Solution.Start()
+AoC2021.Puzzle2.Solution.Start()
---
+++ Puzzle2/Solution.fs
@@ -0,0 +1,31 @@
+﻿module AoC2021.Puzzle2.Solution
+open System.IO
+
+let start_solution_2 () =
+    let mutable stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let mutable horizontal = 0
+    let mutable vertical = 0
+    
+    let fileLines = File.ReadLines("Puzzle2\input.txt")
+    let data =
+            fileLines
+            |> Seq.mapi (fun index x -> x.Split [|' '|])
+            |> Seq.mapi (fun index x -> (x[0], x[1] |> int))
+
+    for direction, amount in data do
+        match (direction, amount) with
+        | (direction, amount) when direction = "forward" -> horizontal <- horizontal + amount
+        | (direction, amount) when direction = "up" -> vertical <- vertical - amount
+        | (direction, amount) when direction = "down" -> vertical <- vertical + amount
+        | _ -> ()
+    
+    printfn $"  Answer:"
+    printfn $"      Horizontal:             %A{horizontal}"
+    printfn $"      Vertical:               %A{vertical}"
+    printfn $"      Vertical & Horizontal:  %A{horizontal * vertical}"
+    printfn $"  Duration: %f{stopWatch.Elapsed.TotalMilliseconds} ms"
+
+let Start () =
+    printfn "== Solution 2 == "
+    printfn "What do you get if you multiply your final horizontal position by your final depth?"
+    start_solution_2()
