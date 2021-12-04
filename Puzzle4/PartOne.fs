e5a915d5a9373b9c39926939e5bddb0d18ae00c8
dimitry@rebelit.nl <dimitry@rebelit.nl>
2021-12-04 12:14:20 +0100

    Added start for Puzzle 4

---
+++ Puzzle4/PartOne.fs
@@ -0,0 +1,17 @@
+﻿module AoC2021.Puzzle4.PartOne
+
+open System
+open System.IO
+
+let Start () =
+    
+    printfn "== Solution 4 | Part One == "
+    printfn "What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)"
+    
+    let mutable stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let fileLines = File.ReadLines("Puzzle4\input.txt")
+    
+    
+    
+    
+    printfn $"  Answer:"
