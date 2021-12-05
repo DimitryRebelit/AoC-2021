29f441cfad26646f7c66182bb3958c0244170990
dimitry@rebelit.nl <dimitry@rebelit.nl>
2021-12-05 15:24:14 +0100

    Added answer for Puzzle 5 Part one and Two

--- Puzzle5/PartOne.fs
+++ Puzzle5/PartOne.fs
@@ -2,42 +2,78 @@
 
 open System.IO
 
-let start() =
+type Point(x : int, y : int) =
+    member val X = x with get, set
+    member val Y = y with get,set
+
+type Line(startPoint : Point, endPoint : Point) =
+    member this.GetLinePoints() : Point[] =
+        let direction = if startPoint.X <> endPoint.X then "horizontal" else "vertical"
+        if direction = "horizontal" then
+            let dif = startPoint.X - endPoint.X
+            let points = Array.zeroCreate ((abs dif + 1))
+            let mutable arrayIndex = 0
+            
+            let startingPoint = min startPoint.X endPoint.X
+            let endpoint = max startPoint.X endPoint.X
+            
+            for i = startingPoint to endpoint do
+                points[arrayIndex] <- new Point(i, startPoint.Y)
+                arrayIndex <- arrayIndex + 1
+            
+            points
+        else
+            let dif = startPoint.Y - endPoint.Y
+            let points = Array.zeroCreate ((abs dif + 1))
+            let mutable arrayIndex = 0
+            
+            let startingPoint = min startPoint.Y endPoint.Y
+            let endpoint = max startPoint.Y endPoint.Y
+            
+            for i = startingPoint to endpoint do
+                points[arrayIndex] <- new Point(startPoint.X, i)
+                arrayIndex <- arrayIndex + 1
+            
+            points
+   
+    member val Start = startPoint with get, set
+    member val End = endPoint with get, set
+   
+
+let Start() =
     printfn "== Solution 5 | Part One == "
+    printfn "At how many points do at least two lines overlap?"
     let mutable stopWatch = System.Diagnostics.Stopwatch.StartNew()
     let fileLines = File.ReadLines("Puzzle5\input.txt")
-    
-    let co = fileLines |> List.mapi (fun i s -> s.Split "->" |> List.map (fun p -> p.Trim()))
+    let co = fileLines |> Seq.mapi (fun i s -> s.Split "->" |> Seq.map (fun p -> p.Trim()))
 
-
-//            // Map them to coordinates
-//            |> List.map (fun positions -> positions
-//                    |> List.map (fun pos ->
-//                        let coordinatesText = pos.Split ','
-//                        coordinatesText
-//                        |> List.map(fun coordinates ->
-//                            let x = coordinates[0] |> int
-//                            let y = coordinates[1] |> int
-//                            
-//                            (x,y))
-//                    )) 
-//                )
+    let lines = co
+                      |> Seq.map (fun coordinates -> coordinates
+                                                    |> Seq.map (fun coordinate -> coordinate.Split ",")
+                                                    |> Seq.map (fun (coordinate:string[]) -> new Point(coordinate.[0] |> int, coordinate.[1] |> int))
+                                                    |> Seq.toArray)
+                      |> Seq.filter(fun (pointPair:Point[]) -> (pointPair[0].X <> pointPair[1].X && pointPair[0].Y <> pointPair[1].Y) <> true)
+                      |> Seq.map (fun pointPair -> new Line(pointPair[0], pointPair[1]))
+                      |> Seq.toArray
+          
+    let gridSize = (max (lines |> Seq.map(fun line -> max line.Start.Y line.End.Y) |> Seq.max) (lines |> Seq.map(fun line -> max line.Start.X line.End.X) |> Seq.max)) + 1
+                     
+    let floor : int [,] = Array2D.zeroCreate gridSize gridSize
     
-    let linesPoints = co |> List.map (fun coordinates ->
-                                coordinates
-                                |> List.map (fun coordinate -> coordinate.Split "," |> Array.toList)
-                                |> List.map (fun coordinate ->
-                                    let x = coordinate.[0] |> int
-                                    let y = coordinate.[1] |> int
-                                    (x,y)
-                                ))
-            
-    let lines = linesPoints |> List.filter(fun points -> points.[0][0] <> points.[1][0]
-            
+    for line in lines do
+        let points = line.GetLinePoints()
+        for i = 0 to (points.Length - 1) do
+            floor[points[i].Y,points[i].X] <- floor[points[i].Y,points[i].X] + 1
+    
+    let mutable intersections = 0
+    
+    for i = 0 to (gridSize - 1) do
+        floor[i,*] |> Array.iter(fun (value:int) ->
+            if value > 1 then intersections <- intersections + 1)
         
-        )
-    
-    printfn "%A" lines
-    
     printfn $"  Answer:"
+    printfn $"       Drawn lines: %A{lines.Length}"
+    printfn $"       Floor size: %A{gridSize} x %A{gridSize}"
+    printfn $"       Intersecting points: %A{intersections}"  
+    printfn $"  Duration: %f{stopWatch.Elapsed.TotalMilliseconds} ms"
         
---
+++ Puzzle5/PartTwo.fs
@@ -0,0 +1,94 @@
+﻿module AoC2021.Puzzle5.PartTwo
+
+open System.IO
+
+type Point(x : int, y : int) =
+    member val X = x with get, set
+    member val Y = y with get,set
+
+type Line(startPoint : Point, endPoint : Point) =
+    member this.GetLinePoints() : Point[] =
+        if this.Direction = "horizontal" then
+            this.DrawDirectionX <- if startPoint.X < endPoint.X then "LeftToRight" else "RightToLeft"
+            
+            let amountOfPointsInLine = ((abs (startPoint.X - endPoint.X)))
+            let points = Array.zeroCreate (amountOfPointsInLine + 1)
+            
+            for i = 0 to amountOfPointsInLine do
+                let pointX = if this.DrawDirectionX = "LeftToRight" then startPoint.X + i else startPoint.X - i
+                let pointY = startPoint.Y
+                points[i] <- new Point(pointX, pointY)
+            
+            points
+        else if this.Direction = "vertical" then
+            this.DrawDirectionY <- if startPoint.Y < endPoint.Y then "TopToBottom" else "BottomToTop" 
+            
+            let amountOfPointsInLine = ((abs (startPoint.Y - endPoint.Y)))
+            let points = Array.zeroCreate (amountOfPointsInLine + 1)
+            
+            for i = 0 to amountOfPointsInLine do
+                let pointX = startPoint.X
+                let pointY = if this.DrawDirectionY = "TopToBottom" then startPoint.Y + i else startPoint.Y - i
+                points[i] <- new Point(pointX, pointY)
+            
+            points
+        else
+            // Get direction
+            this.DrawDirectionX <- if startPoint.X < endPoint.X then "LeftToRight" else "RightToLeft"
+            this.DrawDirectionY <- if startPoint.Y < endPoint.Y then "TopToBottom" else "BottomToTop" 
+            
+            let amountOfPointsInLine = ((abs (startPoint.Y - endPoint.Y)))
+            let points = Array.zeroCreate (amountOfPointsInLine + 1)
+            
+            for i = 0 to amountOfPointsInLine do
+                let pointX = if this.DrawDirectionX = "LeftToRight" then startPoint.X + i else startPoint.X - i
+                let pointY = if this.DrawDirectionY = "TopToBottom" then startPoint.Y + i else startPoint.Y - i
+                points[i] <- new Point(pointX, pointY)
+            
+            points
+   
+    member val DrawDirectionX = "" with get, set
+    member val DrawDirectionY = "" with get, set
+    member val Start = startPoint with get, set
+    member val End = endPoint with get, set
+    member val Direction =  if startPoint.X <> endPoint.X && startPoint.Y <> endPoint.Y then "diagonal" else
+                            if startPoint.X <> endPoint.X then "horizontal" else "vertical"
+
+let Start() =
+    printfn "== Solution 5 | Part Two == "
+    printfn "At how many points do at least two lines overlap including diagonal?"
+    let mutable stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let fileLines = File.ReadLines("Puzzle5\input.txt")
+
+    //   Get lines from input
+    let lines = fileLines
+                      |> Seq.mapi (fun i s -> s.Split "->" |> Seq.map (fun p -> p.Trim()))
+                      |> Seq.map (fun coordinates -> coordinates
+                                                    |> Seq.map (fun coordinate -> coordinate.Split ",")
+                                                    |> Seq.map (fun (coordinate:string[]) -> new Point(coordinate.[0] |> int, coordinate.[1] |> int))
+                                                    |> Seq.toArray)
+                      |> Seq.map (fun pointPair -> new Line(pointPair[0], pointPair[1]))
+                      |> Seq.toArray
+      
+    // Setup grid size    
+    let gridSize = (max (lines |> Seq.map(fun line -> max line.Start.Y line.End.Y) |> Seq.max) (lines |> Seq.map(fun line -> max line.Start.X line.End.X) |> Seq.max)) + 1 
+    let floor : int [,] = Array2D.zeroCreate gridSize gridSize
+    
+    // Fill the sea floor with those hydrothermal vents
+    for line in lines do
+        let points = line.GetLinePoints()
+        for i = 0 to (points.Length - 1) do
+            floor[points[i].Y,points[i].X] <- floor[points[i].Y,points[i].X] + 1
+    
+    // Check for vents
+    let mutable intersections = 0
+    for i = 0 to (gridSize - 1) do
+        floor[i,*] |> Array.iter(fun (value:int) ->
+            if value > 1 then intersections <- intersections + 1)
+
+    printfn $"  Answer:"
+    printfn $"       Drawn lines: %A{lines.Length}"
+    printfn $"       Floor size: %A{gridSize} x %A{gridSize}"
+    printfn $"       Intersecting points: %A{intersections}"  
+    printfn $"  Duration: %f{stopWatch.Elapsed.TotalMilliseconds} ms"
+        
